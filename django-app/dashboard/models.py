2f261f17e998af53d124d29f35ade912251c235c
Santiago Pereira <santiagopereira@MBP-de-Santiago.Home>
2024-11-06 00:33:03 +0000

    fix: correção de alguns documentos

--- django-app/dashboard/models.py
+++ django-app/dashboard/models.py
@@ -2,14 +2,19 @@ from django.db import models
 from django.conf import settings
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        
+
+    )
 
     def __str__(self):
         return self.name
 
 class Phrase(models.Model):
     author = models.CharField(max_length=30, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)  
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='phrases')   
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
