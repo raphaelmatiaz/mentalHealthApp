ad5f2a5c98092db35e47708885c986fcaae6f184
Spereira05 <vieirasantiago205@gmail.com>
2024-11-08 19:19:14 +0000

    feat(django-app): sql.

---
+++ django-app/initdb.d/insert_into_categories.sql
@@ -0,0 +1,13 @@
+INSERT INTO dashboard_category (name) VALUES
+    ('Sadness'),
+    ('Stress at work'),
+    ('Guilt'),
+    ('Body Image'),
+    ('Social Anxiety'),
+    ('Loneliness'),
+    ('Self-Doubt'),
+    ('Anxiety'),
+    ('Anger management'),
+    ('Procrastination'),
+    ('Impostor syndrome');
+
