31025f2ffc8e3d317e5f40863ca10e43a6c46a4b
Spereira05 <vieirasantiago205@gmail.com>
2024-11-11 21:51:04 +0000

    fix(views.py): limpeza de funções desnecessárias

--- django-app/dashboard/views.py
+++ django-app/dashboard/views.py
@@ -1,7 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import CategorySerializer, PhraseSerializer
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Phrase
 from .forms import PhraseForm
@@ -10,42 +6,6 @@ from django.http import Http404
 def home(request):
     return render(request, 'home.html')
 
-class CategoryListView(APIView):
-    def get(self, request):
-        category = Category.objects.all()
-        serializer = CategorySerializer(category, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class PhraseListView(APIView):
-    def get(self, request, category_id):
-        try:
-            category = Category.objects.get(id=category_id)
-            Phrase.objects.filter(category=category)
-            phrase = category.Phrase.first()
-            if phrase:
-                serializer = PhraseSerializer(phrase)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response({"detail": "No phrases found for this category"}, status=status.HTTP_404_NOT_FOUND)
-        except Category.DoesNotExist:
-            return Response({"detail": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
-    
-# def phrase_list(request, category_id):
-#     category = Category.objects.get(id=category_id)
-#     phrases = Phrase.objects.filter(category=category)
-#     return render(request, 'dashboard/phrase_list.html', {'category': category, 'phrases': phrases})
-
-
-from rest_framework import serializers
-
-class PhraseCreateView(APIView):
-    def post(self, request):
-        serializer = PhraseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 def category_list(request): 
     categories = [
         {"id": 1, "name": "Sadness"},
