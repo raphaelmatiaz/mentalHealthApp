a5ce5b0d438bf307a1933bbd424a4477db9c3db0
Spereira05 <vieirasantiago205@gmail.com>
2024-11-12 20:34:27 +0000

    chore: limpeza de código

--- django-app/dashboard/urls.py
+++ django-app/dashboard/urls.py
@@ -11,7 +11,5 @@ urlpatterns = [
     path('phrases/<int:category_id>/', views.phrase_list, name='phrase_list'),  
     # path('like/<int:phrase_id>/', views.like_phrase, name='like_phrase'),
     # path('delete/<int:phrase_id>/', views.delete_phrase, name='delete_phrase'),
-    path('frontendbuild/', views.frontendbuild, name='phrase_list'),
 ]
 
-    #create-phrase/<int:category_id>/
